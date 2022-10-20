a27d0822ba0c939d031819ef5090764d43116d38
Ramon Queiroz dos Santos <ramonqueirozs@gmail.com>
2022-10-20 15:36:26 -0300

    cria elementos na tela inicial

--- LoginScreenViewCode/LoginScreenViewCode/Login/Controller/ViewController.swift
+++ LoginScreenViewCode/LoginScreenViewCode/Login/Controller/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	var loginView: LoginView?
+	
+	override func loadView() {
+		loginView = LoginView()
+		view = loginView
+	}
+		
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
-
-
 }
 
